9b95f008cbd3c492834ae16e471dc833d30c89c9
xionggao <yanxinyu@terminus.io>
2017-05-11 22:25:33 +0800

    添加Application入口

--- src/main/java/com/xg/java/springboot/Application.java
+++ src/main/java/com/xg/java/springboot/Application.java
@@ -1,7 +1,20 @@
 package com.xg.java.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by xionggao on 2017/5/11.
  */
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
+    public static void main (String[] args){
+        SpringApplication.run(Application.class);
+    }
 }
--- src/main/java/com/xg/java/springboot/controller/UserController.java
+++ src/main/java/com/xg/java/springboot/controller/UserController.java
@@ -1,7 +1,21 @@
 package com.xg.java.springboot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * Created by xionggao on 2017/5/11.
  */
+@RestController
+@EnableWebMvc
+@RequestMapping(value = "/api/user/")
 public class UserController {
+
+    @RequestMapping(value = "hello")
+    public String hello (){
+        return "hello world";
+    }
+
+
 }
